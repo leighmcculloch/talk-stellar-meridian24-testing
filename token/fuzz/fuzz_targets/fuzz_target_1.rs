47dd53be7e36f0559b4b8f7153f5518beaae582a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-10-01 00:48:09 +1000

    wip

--- token/fuzz/fuzz_targets/fuzz_target_1.rs
+++ token/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -3,18 +3,25 @@
 use libfuzzer_sys::fuzz_target;
 use soroban_sdk::*;
 
-fuzz_target!(|transfer: i128| {
+#[derive(Arbitrary)]
+pub struct Input {
+    pub a: i128,
+    pub b: i128,
+    pub amount: i128,
+}
+
+fuzz_target!(|input: Input| {
     let env = Env::default();
     let admin = Address::generate(&env);
     let id = env.register(Token, (&admin,));
     let token = TokenClient::new(&env, &id);
 
     let a = Address::generate(&env);
-    token.mock_all_auths().mint(&a, &10);
+    token.mock_all_auths().mint(&a, &input.a);
     let b = Address::generate(&env);
-    token.mock_all_auths().mint(&b, &0);
+    token.mock_all_auths().mint(&b, &input.b);
 
-    let result = token.mock_all_auths().try_transfer(&a, &b, &transfer);
+    let result = token.mock_all_auths().try_transfer(&a, &b, &input.amount);
     match result {
         Ok(Ok(())) => {}
         Ok(Err(_)) => panic!("unexpected type"),
